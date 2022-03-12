e7324f6b5340fd6f52be4922fdb37dc62292ed9c
Bruno Totoli <santosbruno101062@gmail.com>
2022-03-11 21:47:22 -0300

    add basic fields

---
+++ src/boardgame/Board.java
@@ -0,0 +1,43 @@
+package boardgame;
+
+public class Board {
+
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public Piece piece(int row, int columns) {
+        return pieces[row][columns];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+}
