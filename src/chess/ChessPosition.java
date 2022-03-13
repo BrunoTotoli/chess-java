9836adbfc09e1b642bf90b385d976dbe1734992e
Bruno Totoli <santosbruno101062@gmail.com>
2022-03-13 11:26:49 -0300

    Chess exception and chess position

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,38 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+       if(column < 'a' || column > 'h' || row < 1 || row > 8){
+           throw new ChessException("ChessPosition invalid");
+       }
+        this.column = column;
+        this.row = row;
+    }
+
+    protected Position toPosition(){
+        return new Position(8 - row, column -'a');
+    }
+
+    protected static ChessPosition formPosition(Position position){
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    public String toString() {
+        return ""+ column + row;
+    }
+}
